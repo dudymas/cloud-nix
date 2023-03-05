de95ce00b0704f2cee590737ac8fbfc9b29ff14f
Jeremy White <jeremy.white@cloudposse.com>
2023-03-05 06:29:26 -0500

    ref(geodesix,project-templates): change to default nix projects

--- flake.nix
+++ flake.nix
@@ -4,6 +4,12 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs";
+
+    # project-templates
+    mach-nix.url = "github:/DavHau/mach-nix?ref=3.5.0";
+    poetry2nix.url = "github:nix-community/poetry2nix";
+    
+    # geodesix
   };
 
   outputs =
@@ -13,15 +19,24 @@
     }:
     flake-utils.lib.eachDefaultSystem (system:
     let
-      overlays = [];
+      overlays = [ ];
 
       pkgs = import nixpkgs { inherit overlays system; };
 
     in
     {
-      devShells.default = pkgs.mkShell {
-        packages = with pkgs; [ jq yq-go ];
+      devShells = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ jq yq-go ];
+        };
+        project-templates = import ./project-templates { inherit pkgs; };
       };
-      apps = {};
+      apps = { };
+      templates = {
+        geodesix = {
+          path = ./templates/geodesix;
+          description = "Geodesix template for adding nix to your infra repo";
+        };
+      };
     });
 }
--- project-templates/default.nix
+++ project-templates/default.nix
@@ -1,13 +1,6 @@
 {
   description = "Cookiecutter : flake for templating projects";
 
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    mach-nix.url = "github:/DavHau/mach-nix?ref=3.5.0";
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    poetry2nix.url = "github:nix-community/poetry2nix";
-  };
-
   outputs =
     { self
     , flake-utils
